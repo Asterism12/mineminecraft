a9a8c186bdffcb386697e33790a893cda6928cfe
jack <49606038+592402095@users.noreply.github.com>
2019-05-29 23:35:14 +0800

    Create square.java

---
+++ square.java
@@ -0,0 +1,12 @@
+package thing;
+
+public abstract class square {
+	int id;
+	int break_level;
+	boolean liquid=false;
+	boolean pile=true;
+	boolean food=false;
+	boolean takeup=true;
+	boolean through=false;
+	boolean putdown=true;
+}
