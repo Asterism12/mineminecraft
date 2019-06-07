4825bfafd47e80d2bb98f8734a9156ecee77b4ce
Qsort <awpfriend@sina.com>
2019-06-07 18:15:07 +0800

    add slowTimer

--- src/Game/World.java
+++ src/Game/World.java
@@ -29,6 +29,7 @@ public class World {
     static Point canvasLocation = new Point(0, 0);
 
     public static java.util.Timer timer=new java.util.Timer();//物块更新Timer
+    public static java.util.Timer slowTimer=new java.util.Timer();//慢的Timer，用于更新动物行走方向等
     public static Player player;//玩家类
     public static AnimalState animalState=new AnimalState();//动物类
     public static Point.Double startLocation = new Point.Double(2048, 127.99);
@@ -239,6 +240,13 @@ public class World {
                 player.updatePlayer();
             }
         },0,World.FPS);
+
+        slowTimer.schedule(new java.util.TimerTask(){
+            @Override
+            public void run() {
+                //do some thing
+            }
+        },0,5000);
 
         ActionListener update = new ActionListener() {//刷新世界
             @Override
